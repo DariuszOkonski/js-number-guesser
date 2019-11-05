36322d1c114f84015170521df93bb18b46b49de6
Dariusz Okonski <darek200180@gmail.com>
2019-11-05 22:48:48 +0000

    add random numbers to NumberGuesser

--- js/numberguesser.js
+++ js/numberguesser.js
@@ -1,9 +1,14 @@
+const min = 0;
+const max = 10;
+const minAttempts = 3;
+const maxAggempts = 6;
+
 class NumberGuesser {
   constructor() {
-    this.minNumber = 5;
-    this.maxNumber = 10;
-    this.attemps = 3;
-    this.secretNumber = 6;
+    this.minNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    this.maxNumber = (Math.floor(Math.random() * (max - min + 1)) + min) + this.minNumber;
+    this.attemps = Math.floor(Math.random() * (maxAggempts - minAttempts + 1)) + minAttempts;
+    this.secretNumber = Math.floor(Math.random() * (this.maxNumber - this.minNumber + 1)) + this.minNumber;
     this.gameOver = false;
   }
 
@@ -17,5 +22,3 @@ class NumberGuesser {
 }
 
 export default NumberGuesser
-
-// Math.floor(Math.random() * (max - min + 1)) + min;
